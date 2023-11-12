06480adae393f221dd40cb5bea8d8da88c427dd3
Nathan <betweenthieves@gmail.com>
2023-11-12 14:06:53 -0600

    feat: initial solution to 17

--- 17.letter-combinations-of-a-phone-number.ts
+++ 17.letter-combinations-of-a-phone-number.ts
@@ -5,7 +5,32 @@
  */
 
 // @lc code=start
-function letterCombinations(digits: string): string[] {}
+function letterCombinations(digits: string): string[] {
+  const mapping: Record<string, string[]> = {
+    "2": ["a", "b", "c"],
+    "3": ["d", "e", "f"],
+    "4": ["g", "h", "i"],
+    "5": ["j", "k", "l"],
+    "6": ["m", "n", "o"],
+    "7": ["p", "q", "r", "s"],
+    "8": ["t", "u", "v"],
+    "9": ["w", "x", "y", "z"],
+  };
+
+  function addLettersForDigit(digit: string, current: string) {
+    if (!digit) return [];
+    const letters = mapping[digit];
+    const results = letters.map((l) => l + current);
+    return results;
+  }
+
+  let results: string[] = addLettersForDigit(digits[digits.length - 1], "");
+  for (let i = digits.length - 2; i >= 0; i--) {
+    results = results.flatMap((r) => addLettersForDigit(digits[i], r));
+    console.log("results: ", results);
+  }
+  return results;
+}
 // @lc code=end
 
 describe("letterCombinations", () => {
@@ -23,6 +48,6 @@ describe("letterCombinations", () => {
       output: ["a", "b", "c"],
     },
   ])("should turn $input into $output", (testCase) => {
-    expect(letterCombinations(testCase.input)).toEqual(testCase.output);
+    expect(letterCombinations(testCase.input).sort()).toEqual(testCase.output);
   });
 });
