580da790bc0eac15cb40e442c29290e02109d31e
Nathan <betweenthieves@gmail.com>
2023-11-11 19:52:19 -0600

    refactor: improve time complexity of two-sum solution

--- 1.two-sum.ts
+++ 1.two-sum.ts
@@ -5,12 +5,19 @@
  */
 
 // @lc code=start
-export function twoSum(nums: number[], target: number): number[] {
-  for (let i1 = 0; i1 < nums.length; i1++) {
-    for (let i2 = i1 + 1; i2 < nums.length; i2++) {
-      if (nums[i1] + nums[i2] === target) return [i1, i2];
-    }
+function twoSum(nums: number[], target: number): number[] {
+  const indices: Record<number, number> = {};
+  for (let i = 0; i < nums.length; i++) {
+    indices[nums[i]] = i;
   }
+
+  for (let i = 0; i < nums.length; i++) {
+    const complimentValue = target - nums[i];
+    const complimentIndex = indices[complimentValue];
+    if (i !== complimentIndex && complimentIndex !== undefined)
+      return [i, complimentIndex];
+  }
+
   throw new Error("Did not find a solution. This should not occur.");
 }
 // @lc code=end
