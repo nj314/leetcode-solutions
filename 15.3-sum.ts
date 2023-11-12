430953dfa460ebdfff2f37658fccefd3a04e81c6
Nathan Jones <nathanjones314@gmail.com>
2023-11-12 12:53:56 -0600

    refactor: improve time complexity of solution to 15

--- 15.3-sum.ts
+++ 15.3-sum.ts
@@ -11,28 +11,20 @@ function threeSum(unsortedNums: number[]): number[][] {
   // console.log("sorted: ", nums);
 
   for (let i = 0; i < nums.length - 2; i++) {
+    if (i !== 0 && nums[i] === nums[i - 1]) continue; // skip duplicate i values
     let j = i + 1,
       k = nums.length - 1;
 
-    while (j !== k && k > i) {
+    while (j < k) {
       // console.log("indices ", i, j, k);
       const sum = nums[i] + nums[j] + nums[k];
       // console.log(`trying ${nums[i]} + ${nums[j]} + ${nums[k]} = ${sum}`);
       if (sum === 0) {
         // console.log("success");
-        const triplet = [nums[i], nums[j], nums[k]].sort((a, b) => a - b);
-        if (
-          !results.some(
-            (r) =>
-              r[0] === triplet[0] && r[1] === triplet[1] && r[2] === triplet[2]
-          )
-        ) {
-          results.push(triplet);
-        }
-        if (j < i - 1) {
+        results.push([nums[i], nums[j], nums[k]]);
+        j++, k--;
+        while (j < k && nums[j] === nums[j - 1]) {
           j++;
-        } else {
-          k--;
         }
         continue;
       } else if (sum < 0) {
@@ -48,43 +40,39 @@ function threeSum(unsortedNums: number[]): number[][] {
 // @lc code=end
 
 describe("threeSum", () => {
-  it.each(
-    [
-      {
-        input: [-1, 0, 1, 2, -1, -4],
-        output: [
-          [-1, -1, 2],
-          [-1, 0, 1],
-        ],
-      },
-      {
-        input: [0, 1, 1],
-        output: [],
-      },
-      {
-        input: [0, 0, 0],
-        output: [[0, 0, 0]],
-      },
-      {
-        input: [-1, 0, 1, 2, -1, -4, -2, -3, 3, 0, 4],
-        output: [
-          [-4, 0, 4],
-          [-4, 1, 3],
-          [-3, -1, 4],
-          [-3, 0, 3],
-          [-3, 1, 2],
-          [-2, -1, 3],
-          [-2, 0, 2],
-          [-1, -1, 2],
-          [-1, 0, 1],
-        ],
-      },
-    ].slice(3, 4)
-  )("should turn $input into $output", (testCase) => {
-    const results = threeSum(testCase.input);
-    testCase.output.forEach((result) => {
-      expect(results).toContainEqual(result);
-    });
-    expect(results).toHaveLength(testCase.output.length);
+  it.each([
+    {
+      input: [-1, 0, 1, 2, -1, -4],
+      output: [
+        [-1, -1, 2],
+        [-1, 0, 1],
+      ],
+    },
+    {
+      input: [0, 1, 1],
+      output: [],
+    },
+    {
+      input: [0, 0, 0],
+      output: [[0, 0, 0]],
+    },
+    {
+      input: [-1, 0, 1, 2, -1, -4, -2, -3, 3, 0, 4],
+      output: [
+        [-4, 0, 4],
+        [-4, 1, 3],
+        [-3, -1, 4],
+        [-3, 0, 3],
+        [-3, 1, 2],
+        [-2, -1, 3],
+        [-2, 0, 2],
+        [-1, -1, 2],
+        [-1, 0, 1],
+      ],
+    },
+  ])("should turn $input into $output", (testCase) => {
+    const results = threeSum(testCase.input).sort((a, b) => a[0] - b[0]);
+    results.forEach((triplet) => triplet.sort((a, b) => a - b));
+    expect(results).toEqual(testCase.output);
   });
 });
