6de99f6345e27808be216c61740cdc7a602cbc5f
Nathan Jones <nathanjones314@gmail.com>
2023-11-27 19:03:05 -0600

    refactor: solution to 84 in O(n) after editorial

--- 84.largest-rectangle-in-histogram.ts
+++ 84.largest-rectangle-in-histogram.ts
@@ -6,23 +6,51 @@
 
 // @lc code=start
 function largestRectangleArea(heights: number[]): number {
+  if (heights.length === 1) return heights[0];
   // heights must remain in the same order
   let largestArea = 0;
-  let initial = [...heights];
-  while (initial.length) {
-    const curr = [...initial];
-    while (curr.length) {
-      const min = Math.min(...curr);
-      const area = min * curr.length;
-      if (area > largestArea) largestArea = area;
-      // console.log("curr is", curr, "area is", area);
-      while (true) {
-        curr.pop();
-        if (curr[curr.length - 1] !== min) break;
+  const stack: number[] = [-1];
+  for (let i = 0; i < heights.length; i++) {
+    //console.log(`\ni = ${i}, height = ${heights[i]}`);
+    const lastHeight = heights[i - 1];
+    //console.log("entering while loop");
+    while (
+      stack[stack.length - 1] !== -1 &&
+      heights[stack[stack.length - 1]] > heights[i]
+    ) {
+      //console.log("stack is", stack);
+      //console.log("Popping from stack: index", stack[stack.length - 1]);
+      const topIndex = stack.pop() as number;
+      const height = heights[topIndex];
+      const width = i - stack[stack.length - 1] - 1;
+      //console.log("Height is", height, "width is", width);
+      const area = height * width;
+      if (area > largestArea) {
+        //console.log("New max: ", area);
+        largestArea = area;
       }
     }
-    initial.shift();
+    //console.log(`Exited while loop.`);
+    // //console.log(
+    //   `${heights[stack[stack.length - 1]]} > ${heights[i]} === false`
+    // );
+    //console.log(`Pushing ${i}`);
+    stack.push(i);
   }
+
+  //console.log("handling final index");
+  while (stack[stack.length - 1] !== -1) {
+    const topIndex = stack.pop() as number;
+    const height = heights[topIndex];
+    const width = heights.length - stack[stack.length - 1] - 1;
+    //console.log("Height is", height, "width is", width);
+    const area = height * width;
+    if (area > largestArea) {
+      //console.log("New max: ", area);
+      largestArea = area;
+    }
+  }
+
   return largestArea;
 }
 // @lc code=end
@@ -62,7 +90,7 @@ describe("largestRectangleArea", () => {
         input: [3, 6, 5, 7, 4, 8, 1, 0],
         output: 20,
       },
-    ].slice(7, 8)
+    ].slice(0)
   )("should turn $input into $output", (testCase) => {
     expect(largestRectangleArea(testCase.input)).toEqual(testCase.output);
   });
