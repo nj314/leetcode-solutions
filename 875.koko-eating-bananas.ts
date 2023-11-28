0567ade064f9c234fe0654020ece4e1d59f6adb1
Nathan Jones <nathanjones314@gmail.com>
2023-11-27 23:09:44 -0600

    feat: initial solution to 875

--- 875.koko-eating-bananas.ts
+++ 875.koko-eating-bananas.ts
@@ -7,37 +7,51 @@
 // @lc code=start
 function minEatingSpeed(piles: number[], h: number): number {
   let totalCount = Math.ceil(piles.reduce((sum, curr) => sum + curr, 0));
-  // console.log("totalCount is", totalCount);
-  let k = Math.ceil(totalCount / h);
-  // let attempts = 0;
-  // while (attempts++ < 100) {
-  while (true) {
-    // console.log(`Trying k = ${k}`);
-    let t = 0;
-    for (let i = 0; i < piles.length; i++) {
-      t += Math.ceil(piles[i] / k);
+  // console.log("Total count is", totalCount);
+  let min = Math.ceil(totalCount / h);
+  let max = Math.max(...piles);
+  while (min <= max) {
+    // let attempts = 0;
+    // while (attempts++ < 10 && min <= max) {
+    let k = Math.floor((max + min) / 2);
+    // console.log(`Trying min = ${min}, k = ${k}, max = ${max}`);
+    const time = piles.reduce((sum, curr) => sum + Math.ceil(curr / k), 0);
+    if (time <= h) {
+      // try going slower
+      if (max === k) {
+        // console.log("success by k", k);
+        return k;
+      }
+      max = k;
     }
-    // console.log("  t = ", t);
-    if (t <= h) return k;
-    k++;
+    if (time > h) {
+      // try going faster
+      if (max - min === 1) {
+        return max;
+      } else {
+        min = k;
+      }
+    }
   }
-  // throw new Error("Max attempts exceeded");
+  throw new Error("Exceeded max retries");
 }
 // @lc code=end
 
 describe("minEatingSpeed", () => {
-  it.each([
-    {
-      piles: [3, 6, 7, 11],
-      h: 8,
-      output: 4,
-    },
-    {
-      piles: [30, 11, 23, 4, 20],
-      h: 5,
-      output: 30,
-    },
-  ])("should turn $piles, $h into $output", (testCase) => {
+  it.each(
+    [
+      {
+        piles: [3, 6, 7, 11],
+        h: 8,
+        output: 4,
+      },
+      {
+        piles: [30, 11, 23, 4, 20],
+        h: 5,
+        output: 30,
+      },
+    ].slice(0, 2)
+  )("should turn $piles, $h into $output", (testCase) => {
     expect(minEatingSpeed(testCase.piles, testCase.h)).toEqual(testCase.output);
   });
 });
